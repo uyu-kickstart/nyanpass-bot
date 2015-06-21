0655b2b736dd0f75523bf485540328954dc96587
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-06-22 01:40:40 +0900

    support new nyanpass API

--- scripts/nyanpass.js
+++ scripts/nyanpass.js
@@ -4,29 +4,68 @@
 //   nyanpass, にゃんぱす... - にゃんぱすー(๑¯Δ¯๑)
 
 const
-NYANPASS_URL = 'http://nyanpass.com/count';
+NYANPASS_URL_BASE = 'http://nyanpass.com/',
+NYANPASS_URL_ADD = NYANPASS_URL_BASE + 'add.php',
+NYANPASS_URL_GET = NYANPASS_URL_BASE + 'get?',
+API = {
+  add: NYANPASS_URL_ADD,
+  get: NYANPASS_URL_GET,
+};
+
 
 var
+bloem     = require('bloem.js'),
 util      = require('util'),
 useragent = require('random-useragent'),
 request   = require('superagent');
 
+var
+flow = bloem.Pomp()
+  .connect(setup())
+  .connect(api('add'))
+  .connect(api('get'))
+  .connect(send())
+  .connect(error());
+
 module.exports = function (robot) {
   robot.hear(/((?:[^@]|^)nyanpass|(にゃんぱす|ニャンパス)ー?)/i, function (msg) {
-    var
-    count = ~~(Math.random() * 1000) + 1;
-
-    request
-      .post(NYANPASS_URL + count)
-      .set('Content-Type', 'application/x-www-form-urlencoded')
-      .send({ck: 1})
-      .set('User-Agent', useragent.getRandom())
-      .end(function (err, res) {
-        if (err || !res.ok) {
-          console.log(util.inspect(err));
-          return msg.send('なにかがおかしいのん:\n' + err.message);
-        }
-        msg.send('@' + msg.message.user.name + ': にゃんぱすー(๑¯Δ¯๑)\n' + res.text + '\nhttps://raw.githubusercontent.com/MakeNowJust/nyanpass-cli/master/nyanpass.jpg');
-      });
+    flow.send(msg);
   });
 };
+
+function setup() {
+  return bloem.map(function setupIter(msg) {
+    return {
+      msg: msg,
+    };
+  });
+}
+
+function api(name) {
+  return bloem.map(function apiIter(state, next) {
+    request.post(API[name])
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send({nyan: 'pass'})
+      .set('User-Agent', useragent.getRandom())
+      .end(function apiRequestCallback(err, res) {
+        if (err) return next(err);
+        state[name] = res.body;
+        next(null, state);
+      });
+  });
+}
+
+function send() {
+  return bloem.map(function sendIter(state) {
+    var
+    msg = state.msg;
+    msg.send('@' + msg.message.user.name + ': にゃんぱすー(๑¯Δ¯๑)\n' + state.get.cnt + '\nhttps://raw.githubusercontent.com/MakeNowJust/nyanpass-cli/master/nyanpass.jpg');
+  });
+}
+
+function error() {
+  return bloem.rescue(function errorIter(err) {
+    console.log(util.inspect(err));
+    state.msg.send('なにかがおかしいのん:\n' + err.message);
+  });
+}
